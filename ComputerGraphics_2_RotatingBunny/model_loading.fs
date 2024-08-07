fd53ac178f31411faf9a4a4a0ef396990a46e3c6
Jaemin <59792911+Sonak0930@users.noreply.github.com>
2024-08-07 16:02:07 +0900

    Gamma Correction Applied.

--- ComputerGraphics_2_RotatingBunny/model_loading.fs
+++ ComputerGraphics_2_RotatingBunny/model_loading.fs
@@ -4,9 +4,26 @@ in vec4 color;
 out vec4 FragColor;
 
 
+vec4 GammaCorrection(vec4 color) {
+    vec4 correctedColor;
+    for(int i = 0; i < 3; i++) {
+        if (color[i] <= 0.0031308) {
+            correctedColor[i] = 12.92 * color[i];
+        } else if (color[i] <= 1.0) {
+            correctedColor[i] = 1.055 * pow(color[i], 1.0 / 2.4) - 0.055;
+        } else {
+            correctedColor[i] = color[i]; // Ensure color stays within valid range
+        }
+    }
+    correctedColor.a = color.a; // Preserve alpha channel
+    return correctedColor;
+}
 
 void main()
 {
-    FragColor = color;
-   //FragColor = texture(texture_diffuse1,TexCoords);
+    
+    FragColor = GammaCorrection(color);
+     
+
+    //FragColor = texture(texture_diffuse1,TexCoords);
 }
--- ComputerGraphics_2_RotatingBunny/model_loading.vs
+++ ComputerGraphics_2_RotatingBunny/model_loading.vs
@@ -79,14 +79,14 @@ material mymaterial1 = material(
   vec4(0.2, 0.2, 0.2, 1.0),
   vec4(1.0, 0.8, 0.8, 1.0),
   vec4(1.0, 1.0, 1.0, 1.0),
-  5.0
+  25.0
 );
 
 material mymaterial2 = material(
   vec4(0.2, 0.2, 0.2, 1.0),
   vec4(1.0, 0.8, 0.8, 1.0),
   vec4(1.0, 1.0, 1.0, 1.0),
-  5.0
+  50.0
 );
 
 float specularReflectivity = 0.75;
